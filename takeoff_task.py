f0b3b1f8bc4b3adf5c8c426be54691224584091d
Mathias-Ekstroem <Mathias.ekstroem.larsen@gmail.com>
2018-08-09 18:49:03 +0200

    stuff

--- takeoff_task.py
+++ takeoff_task.py
@@ -1,10 +1,12 @@
 import numpy as np
 from physics_sim import PhysicsSim
 
+
 class TakeoffTask():
     """Task (environment) that defines the goal and provides feedback to the agent."""
-    def __init__(self, init_pose=None, init_velocities=None, 
-        init_angle_velocities=None, runtime=5., target_pos=None):
+
+    def __init__(self, init_pose=None, init_velocities=None,
+                 init_angle_velocities=None, runtime=5., target_pos=None):
         """Initialize a Task object.
         Params
         ======
@@ -15,7 +17,7 @@ class TakeoffTask():
             target_pos: target/goal (x,y,z) position for the agent
         """
         # Simulation
-        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime)
         self.action_repeat = 3
 
         self.state_size = self.action_repeat * 6
@@ -24,41 +26,42 @@ class TakeoffTask():
         self.action_size = 4
 
         # Goal
-        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
 
     def get_reward(self):
-        # TODO: make custom reward function
         """Uses current pose of sim to return reward."""
-        #reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
-        
+
         # we need to punish moving in either x or y since we're interested in just gaining some altitude
         x_punish = abs(self.sim.pose[0] - self.target_pos[0])
         y_punish = abs(self.sim.pose[1] - self.target_pos[1])
 
         # give reward bigger the closer to target
-        altitude_reward = ()
+        altitude_reward = 1 / (abs(self.target_pos[2] - self.sim.pose[2]) + 1)
 
-        #punish_x = abs(self.sim.pose[0] - self.target_pos[0])
-        #punish_y = abs(self.sim.pose[1] - self.target_pos[1])
-        #reward_z = 1.0 - (self.target_pos[2] - self.sim.pose[2])
-        reward_z = 1 / (abs(self.target_pos[2] - self.sim.pose[2]) + 1)
+        # reward_z = 1.0 - (self.target_pos[2] - self.sim.pose[2])
+        # reward_z = 1 / (abs(self.target_pos[2] - self.sim.pose[2]) + 1)
         punish_rot1 = abs(self.sim.pose[3])
         punish_rot2 = abs(self.sim.pose[4])
         punish_rot3 = abs(self.sim.pose[5])
-        #reward_vz = self.sim.v[2]
+        # reward_vz = self.sim.v[2]
         reward_tf = self.sim.time
-        reward = reward_z + 0.1 * reward_tf - 0.1 * (punish_x + punish_y) - 0.1 * (punish_rot1 + punish_rot2 + punish_rot3) 
+        # reward = reward_z + 0.1 * reward_tf - 0.1 * (punish_x + punish_y) - 0.1 * (
+        #             punish_rot1 + punish_rot2 + punish_rot3)
+
+        reward = 2 * altitude_reward - 0.01 * (x_punish + y_punish) + 0.1 * self.sim.time
+
+        if self.target_pos[2] == self.sim.pose[2]:
+            reward = 100
 
         return reward
-
 
     def step(self, rotor_speeds):
         """Uses action to obtain next state, reward, done."""
         reward = 0
         pose_all = []
         for _ in range(self.action_repeat):
-            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
-            reward += self.get_reward() 
+            done = self.sim.next_timestep(rotor_speeds)  # update the sim pose and velocities
+            reward += self.get_reward()
             pose_all.append(self.sim.pose)
         next_state = np.concatenate(pose_all)
         return next_state, reward, done
@@ -66,5 +69,5 @@ class TakeoffTask():
     def reset(self):
         """Reset the sim to start a new episode."""
         self.sim.reset()
-        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        state = np.concatenate([self.sim.pose] * self.action_repeat)
         return state
